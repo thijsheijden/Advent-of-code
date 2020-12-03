2621c9fc2135ac302312eccf7972515dec169e48
thijsheijden <hi@thijsheijden.nl>
2020-12-03 11:08:33 +0100

    Day 3 second half of puzzle solution

--- Day 3/main.go
+++ Day 3/main.go
@@ -5,32 +5,63 @@ import (
 	"os"
 )
 
-func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+type slope struct {
+	horizontal       int
+	vertical         int
+	x                int
+	y                int
+	treesEncountered int64
+}
 
-	var treesEncountered int64 = 0
+var slopes []slope
+
+func main() {
+
 	var verticalDistance int = 0
-	var horizontalDistance int = -3
+
+	slopes = append(slopes, slope{horizontal: 1, vertical: 1, x: 0, y: 0, treesEncountered: 0},
+		slope{horizontal: 3, vertical: 1, x: 0, y: 0, treesEncountered: 0},
+		slope{horizontal: 5, vertical: 1, x: 0, y: 0, treesEncountered: 0},
+		slope{horizontal: 7, vertical: 1, x: 0, y: 0, treesEncountered: 0},
+		slope{horizontal: 1, vertical: 2, x: 0, y: 0, treesEncountered: 0})
+
+	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		// Move in the defined slope
+		for i := 0; i < len(slopes); i++ {
+			slope := &slopes[i]
+
+			if verticalDistance%slope.vertical == 0 {
+				// Determine if we need to wrap our horizontal distance
+				if slope.x >= len(line) {
+					slope.x = slope.x % len(line)
+				}
+
+				// Determine if we encounter a tree
+				if line[slope.x] == '#' {
+					slope.treesEncountered++
+				}
+
+				// Update slope
+				slope.x += slope.horizontal
+				slope.y += slope.vertical
+			}
+		}
+
+		// Update vertical distance by one
 		verticalDistance++
-		horizontalDistance += 3
-
-		// Check if the horizontal distance is larger than the line length
-		if horizontalDistance >= len(line) {
-			horizontalDistance = horizontalDistance % len(line)
-		}
-
-		println(line[horizontalDistance])
-
-		// Determine if we encountered a tree
-		if line[horizontalDistance] == '#' {
-			treesEncountered++
-		}
 	}
 
-	println(treesEncountered)
+	println(slopes[0].treesEncountered)
+
+	var totalEncounteredTrees int64 = 1
+
+	// Multiply all encountered trees
+	for _, slope := range slopes {
+		totalEncounteredTrees *= slope.treesEncountered
+	}
+
+	println(totalEncounteredTrees)
 }
