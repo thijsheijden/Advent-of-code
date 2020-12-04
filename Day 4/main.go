1bea4d0c2cef8925c843635d64244223c3dda13b
thijsheijden <hi@thijsheijden.nl>
2020-12-04 11:15:59 +0100

    Day 4 second half of puzzle

--- Day 4/main.go
+++ Day 4/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"os"
+	"regexp"
 	"strings"
 
 	"github.com/fatih/set"
@@ -36,7 +37,11 @@ func main() {
 		splitOnSpace := strings.Split(line, " ")
 		for _, s := range splitOnSpace {
 			if len(s) > 1 {
-				elementsInThisPassport.Add(strings.Split(s, ":")[0])
+				// Validate the value for this key
+				kv := strings.Split(s, ":")
+				if validateValue(kv[0], kv[1]) {
+					elementsInThisPassport.Add(kv[0])
+				}
 			}
 		}
 	}
@@ -47,4 +52,33 @@ func main() {
 	}
 
 	println(numberOfValidPassports)
+}
+
+func validateValue(key string, value string) bool {
+	switch key {
+	case "byr":
+		regex := regexp.MustCompile(`(19[2-8][0-9]|199[0-9]|200[0-2])`)
+		return regex.Match([]byte(value))
+	case "iyr":
+		regex := regexp.MustCompile(`(201[0-9]|2020)`)
+		return regex.Match([]byte(value))
+	case "eyr":
+		regex := regexp.MustCompile(`(202[0-9]|2030)`)
+		return regex.Match([]byte(value))
+	case "hgt":
+		regex := regexp.MustCompile(`(1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in`)
+		return regex.Match([]byte(value))
+	case "hcl":
+		regex := regexp.MustCompile(`#[0-9a-fA-F]{6}`)
+		return regex.Match([]byte(value))
+	case "ecl":
+		regex := regexp.MustCompile(`amb|blu|brn|gry|grn|hzl|oth`)
+		return regex.Match([]byte(value))
+	case "pid":
+		regex := regexp.MustCompile(`[0-9]{9}`)
+		return regex.Match([]byte(value))
+	case "cid":
+		return true
+	}
+	return false
 }
