2c50aaa035906289293568f7ad661dcfcbf0210d
thijsheijden <hi@thijsheijden.nl>
2020-12-08 11:13:56 +0100

    Day 8 second half of puzzle done

--- Day 8/main.go
+++ Day 8/main.go
@@ -5,6 +5,9 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
+
+	"github.com/thijsheijden/advent_of_code/cmd/timetrack"
 )
 
 type operation struct {
@@ -15,9 +18,14 @@ type operation struct {
 }
 
 var operations []operation
-var accumulator int
 
 func main() {
+	defer timetrack.TimeTrack(time.Now())
+	loadOperations()
+	findBadOperation()
+}
+
+func loadOperations() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	i := 0
@@ -35,11 +43,17 @@ func main() {
 
 		i++
 	}
+}
 
+func executeProgram() (bool, int) {
+	reachedPreviouslyExecutedOp := 0
+	accumulator := 0
 	for i := 0; i < len(operations); i++ {
 		if operations[i].executed {
-			println(accumulator)
-			return
+			reachedPreviouslyExecutedOp++
+			if reachedPreviouslyExecutedOp > 300 {
+				return false, accumulator
+			}
 		}
 		operations[i].executed = true
 		switch operations[i].operation {
@@ -49,4 +63,27 @@ func main() {
 			i += operations[i].value - 1
 		}
 	}
+	return true, accumulator
+}
+
+// Find the bad operation by brute forcing
+func findBadOperation() {
+	highestAccumulatedValue := 0
+	for i := 0; i < len(operations); i++ {
+		switch operations[i].operation {
+		case "jmp":
+			// Try changing this value to nop and see if the program completes
+			operations[i].operation = "nop"
+			executed, accumulatedValue := executeProgram()
+			if executed {
+				if accumulatedValue > highestAccumulatedValue {
+					highestAccumulatedValue = accumulatedValue
+				}
+			} else {
+				// Change back operation
+				operations[i].operation = "jmp"
+			}
+		}
+	}
+	println(highestAccumulatedValue)
 }
