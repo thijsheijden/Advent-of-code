784d02d4fab748005fb0656bfaedf6198cfc5f18
thijsheijden <hi@thijsheijden.nl>
2020-12-08 11:13:38 +0100

    Added time tracking function

---
+++ cmd/timetrack/timetrack.go
@@ -0,0 +1,26 @@
+package timetrack
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"runtime"
+	"time"
+)
+
+// TimeTrack tracks the execution time of a function
+func TimeTrack(start time.Time) {
+	elapsed := time.Since(start)
+
+	// Skip this function, and fetch the PC and file for its parent.
+	pc, _, _, _ := runtime.Caller(1)
+
+	// Retrieve a function object this functions parent.
+	funcObj := runtime.FuncForPC(pc)
+
+	// Regex to extract just the function name (and not the module path).
+	runtimeFunc := regexp.MustCompile(`^.*\.(.*)$`)
+	name := runtimeFunc.ReplaceAllString(funcObj.Name(), "$1")
+
+	log.Println(fmt.Sprintf("%s took %s", name, elapsed))
+}
